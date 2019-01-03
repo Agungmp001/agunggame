6f08a84f336f011024a70544ef503e5d5d512a91
agungcloud <agungmahaputra997@gmail.com>
2019-01-03 22:40:22 +0800

    First commit

---
+++ code0.js
@@ -0,0 +1,18 @@
+gdjs.gameCode = {};
+
+gdjs.gameCode.conditionTrue_0 = {val:false};
+gdjs.gameCode.condition0IsTrue_0 = {val:false};
+
+
+gdjs.gameCode.eventsList0xb0b28 = function(runtimeScene) {
+
+}; //End of gdjs.gameCode.eventsList0xb0b28
+
+
+gdjs.gameCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.gameCode.eventsList0xb0b28(runtimeScene);
+return;
+}
+gdjs['gameCode'] = gdjs.gameCode;
